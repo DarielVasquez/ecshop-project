70cb7215284a39987c0e7addab525389d7f108f8
Dariel Vasquez <darielalejandro10@gmail.com>
2023-06-13 12:43:30 -0600

    added routing to single product and category pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, About, Login, Error, Cart } from "./pages";
+import {
+  Home,
+  About,
+  Login,
+  Error,
+  Cart,
+  CategoryProducts,
+  SingleProduct,
+} from "./pages";
 import { NavigationBar, Footer } from "./components";
 // import "./App.css";
 
@@ -12,6 +20,8 @@ function App() {
         <Route path="about" element={<About />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="cart" element={<Cart />}></Route>
+        <Route path="category/:category" element={<CategoryProducts />}></Route>
+        <Route path="product/:id" element={<SingleProduct />}></Route>
         <Route path="*" element={<Error />}></Route>
       </Routes>
       <Footer />
