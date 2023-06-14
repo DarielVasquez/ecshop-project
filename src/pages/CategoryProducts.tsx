b7d1a8c6e9357ca7d8ddc895104ede1e9cbfefd3
Dariel Vasquez <darielalejandro10@gmail.com>
2023-06-14 14:22:01 -0600

    added category products with loading and error handling

--- src/pages/CategoryProducts.tsx
+++ src/pages/CategoryProducts.tsx
@@ -1,22 +1,101 @@
 import React, { useEffect } from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Spinner } from "react-bootstrap";
 import { useAppSelector, useAppDispatch } from "../app/hook";
 import { fetchCategoryProducts } from "../features/categoryProducts/categoryProductsSlice";
-import { useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { MdAddShoppingCart, MdErrorOutline } from "react-icons/md";
+import { Rating } from "../components";
 
 const CategoryProducts = () => {
   const { category } = useParams();
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const products = useAppSelector((state) => state.categoryProducts.data);
+  const loading = useAppSelector((state) => state.categoryProducts.loading);
+  const error = useAppSelector((state) => state.categoryProducts.error);
 
   useEffect(() => {
-    dispatch(fetchCategoryProducts("electronics"));
-  }, []);
+    if (category) {
+      dispatch(fetchCategoryProducts(category));
+    }
+  }, [category]);
 
   return (
     <main>
       <Container>
-        <Row>{category}</Row>
+        <Card className="p-5">
+          <Row>
+            <Col xs="12" sm="3">
+              Filters
+            </Col>
+            <Col xs="12" sm="9">
+              <Row>
+                {loading ? (
+                  <div className="d-flex justify-content-center">
+                    <Spinner
+                      animation="border"
+                      role="status"
+                      variant="secondary"
+                    >
+                      <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                  </div>
+                ) : error ? (
+                  <div
+                    className="d-flex justify-content-center align-items-center"
+                    style={{ gap: "5px" }}
+                  >
+                    <MdErrorOutline></MdErrorOutline>
+                    Error loading content
+                  </div>
+                ) : (
+                  products?.map((product) => {
+                    const {
+                      category,
+                      description,
+                      id,
+                      price,
+                      rating,
+                      image,
+                      title,
+                    } = product;
+                    return (
+                      <Col xs="6" md="4" lg="3" key={id}>
+                        <Row>
+                          <div className="d-flex justify-content-center pt-3 pb-3">
+                            <img
+                              src={image}
+                              alt={title}
+                              className="category-product-img"
+                              onClick={() => navigate(`/product/${id}`)}
+                            />
+                          </div>
+                          <p style={{ fontWeight: "600", fontSize: "18px" }}>
+                            ${price}
+                          </p>
+                          <p>{title}</p>
+                          <Col className="d-flex flex-row">
+                            <Rating rating={rating.rate}></Rating>
+                            <span
+                              style={{
+                                fontSize: "12px",
+                                paddingLeft: "4px",
+                                paddingTop: "4px",
+                              }}
+                              className="d-flex align-items-center"
+                            >
+                              {rating.count}
+                            </span>
+                          </Col>
+                        </Row>
+                      </Col>
+                    );
+                  })
+                )}
+              </Row>
+            </Col>
+          </Row>
+        </Card>
       </Container>
     </main>
   );
