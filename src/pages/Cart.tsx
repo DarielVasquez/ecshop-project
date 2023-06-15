8939ae47498a64a1e0c3e7a8ef6ffba9a802f75c
Dariel Vasquez <darielalejandro10@gmail.com>
2023-06-14 18:31:26 -0600

    added logo and more styling for cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -11,7 +11,7 @@ import {
   Badge,
   CloseButton,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../app/hook";
 import {
   MdMenu,
@@ -29,6 +29,7 @@ import {
 
 const Cart = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const cartItems = useAppSelector((state) => state.cart.cartItems);
   const totalAmount = useAppSelector((state) => state.cart.totalAmount);
   const subtotalCost = useAppSelector((state) => state.cart.subtotalCost);
@@ -37,95 +38,110 @@ const Cart = () => {
   return (
     <main>
       <Container>
-        <Row>
+        <Row style={{ rowGap: "25px" }}>
           <Col xs="12" md="8">
-            {cartItems?.map((item, i) => {
-              const { id, title, amount, total, image, price, category } = item;
-              return (
-                <Container key={id}>
-                  <Row>
-                    <Col
-                      xs="12"
-                      sm="4"
-                      className="d-flex align-items-center justify-content-center p-2"
-                    >
-                      <img
-                        src={image}
-                        alt={title}
-                        className="cart-img"
-                        style={{
-                          height: "clamp(16vh, 14vh, 12vh)",
-                          width: "clamp(16vh, 14vh, 12vh)",
-                        }}
-                      />
-                    </Col>
-                    <Col
-                      xs="6"
-                      sm="4"
-                      className="d-flex align-items-center p-2"
-                      style={{ whiteSpace: "normal" }}
-                    >
-                      <Row>
-                        <span style={{ padding: "5px", fontWeight: "500" }}>
-                          {title}
-                        </span>
-                        <span
-                          style={{ padding: "5px" }}
-                          className="text-capitalize"
-                        >
-                          {category}
-                        </span>
-                        <span style={{ padding: "5px", fontWeight: "100" }}>
-                          Each: ${price}
-                        </span>
-                        <span
+            <Card className="p-2">
+              {cartItems?.map((item, i) => {
+                const { id, title, amount, total, image, price, category } =
+                  item;
+                return (
+                  <Container key={id}>
+                    <Row>
+                      <Col
+                        xs="12"
+                        sm="4"
+                        className="d-flex align-items-center justify-content-center p-2"
+                      >
+                        <img
+                          src={image}
+                          alt={title}
+                          className="cart-img"
                           style={{
-                            padding: "5px",
+                            height: "clamp(12vh, 15vh, 18vh)",
+                            width: "clamp(12vh, 15vh, 18vh)",
                             cursor: "pointer",
-                            color: "red",
                           }}
-                          onClick={() => dispatch(removeItemFromCart(item))}
-                        >
-                          Remove
-                        </span>
-                      </Row>
-                    </Col>
-                    <Col xs="6" sm="4" className="d-flex justify-content-end">
-                      <Row>
-                        <span
-                          style={{ fontWeight: "600" }}
-                          className="fs-5 d-flex justify-content-end pt-4"
-                        >
-                          ${total !== price && amount === 1 ? price : total}
-                        </span>
-                        <Col className="d-flex align-items-center justify-content-end pt-3 pb-3">
-                          <Button
-                            variant="success"
+                          onClick={() => navigate(`/product/${id - 1}`)}
+                        />
+                      </Col>
+                      <Col
+                        xs="6"
+                        sm="4"
+                        className="d-flex align-items-center p-2"
+                        style={{ whiteSpace: "normal" }}
+                      >
+                        <Row>
+                          <span
                             style={{
-                              fontSize: "clamp(6px, 10px, 12px)",
+                              padding: "5px",
+                              fontWeight: "500",
+                              cursor: "pointer",
                             }}
-                            onClick={() => dispatch(addToCart(item))}
+                            onClick={() => navigate(`/product/${id - 1}`)}
                           >
-                            <MdAdd></MdAdd>
-                          </Button>
-                          <span className="p-2">{amount}</span>
-                          <Button
-                            variant="danger"
+                            {title}
+                          </span>
+                          <span
+                            style={{ padding: "5px" }}
+                            className="text-capitalize"
+                          >
+                            {category}
+                          </span>
+                          <span style={{ padding: "5px", fontWeight: "100" }}>
+                            Each: ${price}
+                          </span>
+                          <span
                             style={{
-                              fontSize: "clamp(6px, 10px, 12px)",
+                              padding: "5px",
+                              cursor: "pointer",
+                              color: "red",
                             }}
-                            onClick={() => dispatch(removeOneFromCart(item))}
+                            onClick={() => dispatch(removeItemFromCart(item))}
                           >
-                            <MdRemove></MdRemove>
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <hr />
-                  </Row>
-                </Container>
-              );
-            })}
+                            Remove
+                          </span>
+                        </Row>
+                      </Col>
+                      <Col xs="6" sm="4" className="d-flex justify-content-end">
+                        <Row>
+                          <span
+                            style={{ fontWeight: "600" }}
+                            className="fs-5 d-flex justify-content-end pt-4"
+                          >
+                            $
+                            {total !== price && amount === 1
+                              ? price
+                              : total.toFixed(2)}
+                          </span>
+                          <Col className="d-flex align-items-center justify-content-end pt-3 pb-3">
+                            <Button
+                              variant="success"
+                              style={{
+                                fontSize: "clamp(6px, 10px, 12px)",
+                              }}
+                              onClick={() => dispatch(addToCart(item))}
+                            >
+                              <MdAdd></MdAdd>
+                            </Button>
+                            <span className="p-2">{amount}</span>
+                            <Button
+                              variant="danger"
+                              style={{
+                                fontSize: "clamp(6px, 10px, 12px)",
+                              }}
+                              onClick={() => dispatch(removeOneFromCart(item))}
+                            >
+                              <MdRemove></MdRemove>
+                            </Button>
+                          </Col>
+                        </Row>
+                      </Col>
+                      <hr />
+                    </Row>
+                  </Container>
+                );
+              })}
+            </Card>
           </Col>
           <Col xs="12" md="4">
             <Card>
